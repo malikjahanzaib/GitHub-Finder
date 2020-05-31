d2cb9b81c92bbf4e99fac4473df6b4421c04cd6d
Malik Jahanzaib <jahanzaibmlk321@gmail.com>
2020-05-31 12:10:19 +0530

    Changed event listner to 'Enter Key'

--- GitHub Finder/app.js
+++ GitHub Finder/app.js
@@ -1,19 +1,21 @@
 const github = new Github();
 const ui = new UI();
 const searchUser = document.getElementById("searchUser");
-searchUser.addEventListener("keyup", (e) => {
-  const userValue = e.target.value;
-  if (userValue !== "") {
-    github.getUser(userValue).then((data) => {
-      if (data.profile.message === "Not Found") {
-        ui.clearProfile();
-        ui.showAlert("User Not Found", "alert alert-danger");
-      } else {
-        ui.showProfile(data.profile);
-        ui.showRepos(data.repos);
-      }
-    });
-  } else {
-    ui.clearProfile();
+searchUser.addEventListener("keypress", (e) => {
+  if (e.keyCode == 13) {
+    const userValue = e.target.value;
+    if (userValue !== "") {
+      github.getUser(userValue).then((data) => {
+        if (data.profile.message === "Not Found") {
+          ui.clearProfile();
+          ui.showAlert("User Not Found", "alert alert-danger");
+        } else {
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
+        }
+      });
+    } else {
+      ui.clearProfile();
+    }
   }
 });
