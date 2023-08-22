9abfe14c386ca8aa60cdcd727144ebd10d2eaf08
Jahanzaib Malik <46221334+malikjahanzaib@users.noreply.github.com>
2023-08-21 19:40:26 -0700

    Updated ui.js

--- GitHub Finder/ui.js
+++ GitHub Finder/ui.js
@@ -7,7 +7,7 @@ class UI {
       <div class="card card-body mb-3">
         <div class="row">
             <div class="col-md-3">
-                <img class="img-fluid mb-2" src="${user.avatar_url}">
+                <img class="img-fluid mb-2" style="border-radius: 10px" src="${user.avatar_url}">
                 <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
             </div>
             <div class="col-md-9">
